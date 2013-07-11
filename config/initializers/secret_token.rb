977b664bbd5ad8918257b71ed288811e85a4cae3
Eric Jacobs <eric@avalanchenine.com>
2013-07-11 01:37:43 -0600

    initial commit... just a brand new rails app.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Latelatelate::Application.config.secret_key_base = '5e369bda6cf99308d13c646616de1b1c67916b52ab78b96b8df8175096fe92e86b0efad4e19eecbcd94bc1625675162bc2d6520c9ee7c4f6e2302b2d1e06fbfb'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Latelatelate::Application.config.session_store :cookie_store, key: '_latelatelate_session'
